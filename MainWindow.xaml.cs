ca18d5765665ac9da1867316596510e868402b7c
anshuman singh <anshumanpbh@gmail.com>
2015-12-25 14:17:18 +0000

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+using System.Windows;
+
+using System.Timers;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static int am_i_connected=1;
+        private static int re_connected = 0;
+        private int is_thread_is_running = 0;
+        public static int is_proxy_enabled=0;
+        public static string proxy_url;
+        public static int port;
+        public static string username;
+        public static string password;
+        public static string config_file_path= @"C:\Users\ansh\Documents\config.txt";
+        public static  int is_file_exist=0;
+        public static string status;
+   Thread thread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += new ElapsedEventHandler(is_net_connected);
+            timer.Interval = 15000;
+            string msg;
+            if (File.Exists(config_file_path))
+            {
+                is_proxy_enabled = 1;
+                is_file_exist = 1;
+                var lines = File.ReadLines(config_file_path);
+                string[] temp = new string[5];
+                int i = 0;
+                foreach (var line in lines)
+                {
+                    temp[i++] = line;
+                }
+
+           status = temp[0];
+                proxy_url = temp[1];
+                port = Convert.ToInt32(temp[2]);
+                username = temp[3];
+                password = temp[4];
+            }
+            timer.Enabled = true;
+
+            if (is_proxy_enabled == 0)
+            listBox.Items.Add("monitoring started , configure the proxy detail\n if you are behind proxy server");
+            else
+
+                listBox.Items.Add("monitoring started ,proxy is configured");
+
+
+
+        }
+
+
+        public void is_net_connected(object source, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(CheckForInternetConnection, System.Windows.Threading.DispatcherPriority.ContextIdle);
+        }
+
+        public void make_alarm()
+        {
+            is_thread_is_running = 1;
+            int i = 0;
+            for(i=0;i<500;i++)
+            {
+                Console.Beep();
+            }
+        }
+
+        public void CheckForInternetConnection()
+        {
+           
+            
+
+            
+
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                    if (is_proxy_enabled == 1)
+                    {
+
+                        WebProxy proxy = new WebProxy(proxy_url+":"+port, true);
+                        proxy.Credentials = new NetworkCredential(username, password);  //These can be replaced by user input, if wanted.
+                                                                                                   // WebRequest.DefaultWebProxy = proxy;
+                        client.Proxy = proxy;
+                    }
+                        using (var stream = client.OpenRead("http://www.google.com"))
+                        {
+                        //listBox.Items.Add("connected at" + " " + DateTime.Now.ToString());
+                        if (am_i_connected == 0 && re_connected == 0)
+                        {
+                            if (is_thread_is_running == 1)
+                            {
+                                is_thread_is_running = 0;
+                                thread.Abort();
+                            }
+                            listBox.Items.Add("connected again at" + " " + DateTime.Now.ToString());
+
+                            am_i_connected = 1;
+                            re_connected = 1;
+                        }
+                        }
+                    }
+                }
+                catch
+                {
+                if (am_i_connected == 1)
+                {
+                    if (is_thread_is_running == 0)
+                    {
+                        thread = new Thread(make_alarm);
+                        thread.Start();
+                    }
+                    listBox.Items.Add("disconnected at" + " " + DateTime.Now.ToString()+"\n"+"waiting for connection.....");
+                    am_i_connected = 0;
+                    re_connected = 0;
+                }
+
+                }
+
+
+
+            
+
+
+
+            }
+
+        private void proxy_clicked(object sender, RoutedEventArgs e)
+        {
+            var w = new Window
+            {
+                Content = new UserControl1(),
+                Title = "proxy",
+                SizeToContent = SizeToContent.WidthAndHeight,
+                ResizeMode = ResizeMode.CanResize
+
+            };
+            w.Show();
+        }
+
+        private void stopalarm_Click(object sender, RoutedEventArgs e)
+        {
+            if (is_thread_is_running == 1)
+            {
+                is_thread_is_running = 0;
+                thread.Abort();
+            }
+            else
+            {
+                MessageBox.Show("alarm is not started yet\n");
+            }
+        }
+    }
+
+
+}
---
+++ UserControl1.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        
+        public UserControl1()
+        {
+            InitializeComponent();
+            if (File.Exists(MainWindow.config_file_path))
+            {
+                
+                var lines = File.ReadLines(MainWindow.config_file_path);
+                string[] temp = new string[5];
+                int i = 0;
+                foreach (var line in lines)
+                {
+                    temp[i++] = line;
+                }
+
+                string status = temp[0];
+                textBox1.Text = temp[1];
+                textBox2.Text = temp[2];
+                textBox3.Text = temp[3];
+                textBox4.Text = temp[4];
+                if (status == "enabled")
+                    checkBox.IsChecked = true;
+            }
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(MainWindow.config_file_path))
+            {
+                File.Delete(MainWindow.config_file_path);
+                string path = MainWindow.config_file_path;
+                File.AppendAllText(path, "disabled" + Environment.NewLine);
+
+                File.AppendAllText(path, textBox1.Text + Environment.NewLine);
+                File.AppendAllText(path, textBox2.Text + Environment.NewLine);
+                File.AppendAllText(path, textBox3.Text + Environment.NewLine);
+                File.AppendAllText(path, textBox4.Text + Environment.NewLine);
+
+            }
+           
+            MainWindow.is_proxy_enabled = 0;
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            string url,u,p;
+            
+            url = textBox1.Text;
+       
+            try
+            {
+                int.TryParse(textBox2.Text,out MainWindow.port);
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+            u = textBox3.Text;
+            p = textBox4.Text;
+
+            if(url.Length==0 || u.Length==0 || p.Length==0)
+            {
+                MessageBox.Show("some field are empty");
+
+            }
+            else
+            {
+               MainWindow.proxy_url = url;
+                MainWindow.username = u;
+                string path = MainWindow.config_file_path;
+               MainWindow.password = p;
+                if (File.Exists(MainWindow.config_file_path))
+                {
+                    File.Delete(MainWindow.config_file_path);
+
+                }
+                File.AppendAllText(path, "enabled" + Environment.NewLine);
+
+                File.AppendAllText(path,url + Environment.NewLine);
+                File.AppendAllText(path, textBox2.Text + Environment.NewLine);
+                File.AppendAllText(path, u + Environment.NewLine);
+                File.AppendAllText(path, p + Environment.NewLine);
+                MainWindow.is_proxy_enabled = 1;
+
+                Window.GetWindow(this).Close();
+
+            }
+        }
+    }
+}
